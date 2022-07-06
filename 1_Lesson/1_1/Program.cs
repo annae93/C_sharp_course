88c6d87b2fabde0f7775eeb80d94d62d35dfb031
Anna <anna.a.enina@yandex.ru>
2022-07-06 14:36:32 +0300

    HW 1_1

--- 1_Lesson/1_1/Program.cs
+++ 1_Lesson/1_1/Program.cs
@@ -1,7 +1,4 @@
-﻿//string num_str = Console.ReadLine();
-//int num = int.Parse(num_str);
-
-int num1 = int.Parse(Console.ReadLine());
+﻿int num1 = int.Parse(Console.ReadLine());
 int num2 = int.Parse(Console.ReadLine());
 if (num2 == num1 * num1)
 {
---
+++ 1_Lesson/HW1_1/Program.cs
@@ -0,0 +1,13 @@
+﻿//Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+int num1 = int.Parse(Console.ReadLine());
+int num2 = int.Parse(Console.ReadLine());
+if (num1 > num2)
+{
+    Console.WriteLine($"The first number {num1} is greater than second number {num2}");
+}
+else
+{
+    Console.WriteLine($"The second number {num2} is greater than first number {num1}");
+}
+
