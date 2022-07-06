6335f63cb33f83a7232638af8cae6c40967860ae
Anna <anna.a.enina@yandex.ru>
2022-07-06 18:22:33 +0300

    Task 2_4

---
+++ 2_Lesson/2_4/Program.cs
@@ -0,0 +1,16 @@
+﻿void OneAndTwo(int num)
+{       
+    if (num % 7 == 0 & num % 23 == 0)
+    {
+        Console.WriteLine($"{num} -> yes");
+    }
+    else
+    {
+        Console.WriteLine($"{num} -> no");        
+    }    
+}
+
+OneAndTwo(14);
+OneAndTwo(46);
+OneAndTwo(161);
+
