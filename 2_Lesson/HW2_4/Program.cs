b10140ce670ef8b14f04d4925db22c21301a439f
Anna <anna.a.enina@yandex.ru>
2022-07-07 01:21:46 +0300

    HW 2_4

---
+++ 2_Lesson/HW2_4/Program.cs
@@ -0,0 +1,18 @@
+﻿//Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+Console.Write ("Enter week day: ");
+string DayOfWeek = Console.ReadLine();
+int dayNum = int.Parse(DayOfWeek);
+if (dayNum == 6)
+{
+    Console.WriteLine("yes");
+}
+else if (dayNum == 7)
+{
+    Console.WriteLine("yes");
+}
+else
+{
+    Console.WriteLine("no");
+}
+
+
