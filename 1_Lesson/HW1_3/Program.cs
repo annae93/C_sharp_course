2aaf9dd5142dd8d8ec169d73159c1c2efe17b75f
Anna <anna.a.enina@yandex.ru>
2022-07-06 15:08:22 +0300

    HW 1_3

---
+++ 1_Lesson/HW1_3/Program.cs
@@ -0,0 +1,11 @@
+﻿//Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без оcтатка).
+int a = int.Parse(Console.ReadLine());
+
+if (a % 2 == 0)
+{
+    Console.WriteLine($" number {a} is integer");
+}
+else
+{
+    Console.WriteLine($" number {a} is non-integer");
+}
