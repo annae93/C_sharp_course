0a47be98e63a774b833843033b1de1802f524f68
Anna <anna.a.enina@yandex.ru>
2022-07-06 18:03:33 +0300

    Task 2_2

---
+++ 2_Lesson/2_2/Program.cs
@@ -0,0 +1,16 @@
+﻿int MaxNumber(int a)
+{
+    Console.WriteLine(a);
+    
+    if (a % 10 > a / 10)
+    {
+        return a % 10;
+    }
+    else
+    {
+        return a / 10;
+    }
+}
+
+
+Console.WriteLine(MaxNumber(new Random().Next(10, 100)));
