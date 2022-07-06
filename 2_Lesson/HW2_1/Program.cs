4d89dd7c8940b035d37644efa705b163998abc08
Anna <anna.a.enina@yandex.ru>
2022-07-06 22:25:31 +0300

    HW 2_1

--- 2_Lesson/HW2_1/Program.cs
+++ 2_Lesson/HW2_1/Program.cs
@@ -1,7 +1,7 @@
 ﻿//Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
-int TakeNum(int num)
+int ThreePartNum(int num)
 {
     return num /  10 % 10;
 }
 
-Console.WriteLine(TakeNum(918));
+Console.WriteLine(ThreePartNum(918));
