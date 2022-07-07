74b24ff047902e918f305691e2db98246698401c
Anna <anna.a.enina@yandex.ru>
2022-07-07 12:30:18 +0300

    Task 3_2

--- 3_Lesson/3_1/Program.cs
+++ 3_Lesson/3_1/Program.cs
@@ -31,36 +31,3 @@ Quaters(-2, -2);
 Quaters(2, -2);
 Quaters(0, -2);
 Quaters(2, 0);
-
-// 1. Напишите программу, которая по заданному номеру четверти,
-//    показывает диапазон возможных координат точек в этой четверти (x и y).
-
-//void Coordinates(string num)
-//{
-    //if (num == "I")
-    //{
-    //    Console.WriteLine("x > 0, y > 0");
-    //}
-    //else if (num == "II")
-    //{
-      //  Console.WriteLine("x < 0, y > 0");
-    //}
-    //else if (num == "III")
-    //{
-    //    Console.WriteLine("x < 0, y < 0");
-    //}
-    //else if (num == "IV")
-    //{
-      //  Console.WriteLine("x > 0, y < 0");
-    //}
-    //else
-    //{
-    //    Console.WriteLine("The data is not correct!");
-    //}
-//}
-
-//Coordinates("I");
-//Coordinates("IV");
-//Coordinates("II");
-//Coordinates("III");
-//Coordinates("X");
