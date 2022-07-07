0b937deed6ea46e39ce2a8d5f674000e88f9125f
Anna <anna.a.enina@yandex.ru>
2022-07-07 15:55:12 +0300

    HW 3_1

---
+++ 3_Lesson/HW3_1/Program.cs
@@ -0,0 +1,21 @@
+﻿//Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+//14212 -> нет
+//12821 -> да
+//23432 -> да
+Console.Write("Enter your number: ");
+string num = Console.ReadLine();
+
+void CheckNum(string num)
+{
+  if (num[0] == num[4] || num[1]==num[3])
+  {
+    Console.WriteLine($"Your number: {num} - is Palindrome.");
+  }
+  else Console.WriteLine($"Your number: {num} - is non-Palindrome.");
+}
+
+if (num.Length == 5)
+{
+  CheckNum(num);
+}
+else Console.WriteLine($"Type correct number");
