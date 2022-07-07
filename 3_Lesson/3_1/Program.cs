868fc50ce2ec35dda2364916020769a8ae3681ab
Anna <anna.a.enina@yandex.ru>
2022-07-07 12:19:33 +0300

    Task 3_1

---
+++ 3_Lesson/3_1/Program.cs
@@ -0,0 +1,66 @@
+﻿// 1. Напишите программу, которая принимает на вход координаты точки (X и Y),
+//    причём X ≠ 0 или Y ≠ 0 и выдаёт номер четверти плоскости, в которой находится эта точка.
+
+void Quaters(int x, int y)
+{
+    if (x == 0 || y == 0)
+    {
+        Console.WriteLine("x = 0, y = 0");
+    }
+    else if (x > 0 & y > 0)
+    {
+        Console.WriteLine("I");
+    }
+    else if (x < 0 & y > 0)
+    {
+        Console.WriteLine("II");
+    }
+    else if (x < 0 & y < 0)
+    {
+        Console.WriteLine("III");
+    }
+    else if (x > 0 & y < 0)
+    {
+        Console.WriteLine("IV");
+    }
+}
+
+Quaters(2, 2);
+Quaters(-2, 2);
+Quaters(-2, -2);
+Quaters(2, -2);
+Quaters(0, -2);
+Quaters(2, 0);
+
+// 1. Напишите программу, которая по заданному номеру четверти,
+//    показывает диапазон возможных координат точек в этой четверти (x и y).
+
+//void Coordinates(string num)
+//{
+    //if (num == "I")
+    //{
+    //    Console.WriteLine("x > 0, y > 0");
+    //}
+    //else if (num == "II")
+    //{
+      //  Console.WriteLine("x < 0, y > 0");
+    //}
+    //else if (num == "III")
+    //{
+    //    Console.WriteLine("x < 0, y < 0");
+    //}
+    //else if (num == "IV")
+    //{
+      //  Console.WriteLine("x > 0, y < 0");
+    //}
+    //else
+    //{
+    //    Console.WriteLine("The data is not correct!");
+    //}
+//}
+
+//Coordinates("I");
+//Coordinates("IV");
+//Coordinates("II");
+//Coordinates("III");
+//Coordinates("X");
