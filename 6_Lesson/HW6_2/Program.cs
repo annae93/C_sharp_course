86805ca1f1ed2bd10819b87f8ee913ceb871d555
Anna <anna.a.enina@yandex.ru>
2022-07-31 16:24:05 +0300

    HW 6_2

---
+++ 6_Lesson/HW6_2/Program.cs
@@ -0,0 +1,12 @@
+﻿// 2. Напишите программу, которая найдёт точку пересечения двух прямых,
+//    заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения
+//    b1, k1, b2 и k2 задаются пользователем.
+
+void Intersection(double b1, double k1, double b2, double k2)
+{
+    double x = (b2 - b1) / (k1 - k2);
+    double y = k1 * x + b1;
+    Console.WriteLine($"Intersection point: ({x}; {y})");
+}
+
+Intersection(8, 12, 99, 14);
