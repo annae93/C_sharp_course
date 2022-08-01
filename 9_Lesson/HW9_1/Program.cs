e34c925a9841da75fbef675e8a41f90d3fc5b328
Anna <anna.a.enina@yandex.ru>
2022-08-01 22:08:19 +0300

    HW 9_1

---
+++ 9_Lesson/HW9_1/Program.cs
@@ -0,0 +1,18 @@
+﻿int m = InputNumbers("Enter m: ");
+int n = InputNumbers("Enter n: ");
+int count = n / 2;
+PrintNumber(n, count);
+
+void PrintNumber(int n, int count)
+{
+  if (count > n) return;
+  PrintNumber(n, count + 2);
+  Console.Write(count + ", ");
+}
+
+int InputNumbers(string input) 
+{
+  Console.Write(input);
+  int output = Convert.ToInt32(Console.ReadLine());
+  return output;
+}
